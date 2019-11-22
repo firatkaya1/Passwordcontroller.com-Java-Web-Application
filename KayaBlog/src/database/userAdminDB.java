5f394076752232a23408e1377a6d37fa39ca2e76
firatkaya1 <firat.kaya@mail.com>
2019-11-22 15:37:26 +0300

    test

--- KayaBlog/src/database/userAdminDB.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public interface userAdminDB {
-	
-	String username = "superAdmin";
-	String password = "bE99321605848"; 
-	String userAdminDB = "jdbc:mysql://178.128.201.205/newsdata";
-
-}
